b07d978f3299a15b0837b5209dfb75c20f753338
Maria Mihaleva <mabrasheva1@gmail.com>
2023-07-08 23:48:27 +0300

    Added model "Article".

--- django_blogging_app/apps/article/models.py
+++ django_blogging_app/apps/article/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(
+        blank=False,
+        null=False,
+    )
+    text = models.TextField(
+        blank=False,
+        null=False,
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_on = models.DateTimeField(
+        auto_now=True,
+    )
+    # ToDo
+    # author = models.OneToOneField()
