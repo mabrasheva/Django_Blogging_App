0c029ed2bc599c3d428e4a2bc00635bf9f4db546
Maria Mihaleva <mabrasheva1@gmail.com>
2023-07-28 18:27:24 +0300

    recreated all the migrations because of issues with unit tests

--- django_blogging_app/apps/user_profile/tests.py
+++ django_blogging_app/apps/user_profile/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class LoginUserViewTests(TestCase):
+    def test_login(self):
+        pass
+
+    def test_second(self):
+        pass
